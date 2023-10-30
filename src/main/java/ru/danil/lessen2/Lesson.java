50e94846e663b5ce2ef2bfdf14d095c7a4e93f34
danil <danilalait@mail.ru>
2023-10-30 19:59:52 -0200

    lessen2

---
+++ src/main/java/ru/danil/lessen2/Lesson.java
@@ -0,0 +1,33 @@
+package ru.danil.lessen2;
+
+public class Lesson {
+    public static void main(String[] args) {
+
+    }
+    public boolean within10and20(int a, int b) {
+        int sum = a + b;
+        return sum >= 10 && sum <= 20;
+    }
+
+    public static  void isPozitiveOrNegative(int t) {
+        if (t >= 0) {
+            System.out.println("положительное число");
+        } else {
+            System.out.println("отрицательное число");
+        }
+    }
+
+    public static boolean isNegative(int a) {
+        return a < 0;
+    }
+
+    public static void printWordNTimes(String word, int times) {
+        for (int i = 0; i < times; i++) {
+            System.out.println(word);
+        }
+    }
+}
+
+
+
+
---
+++ src/main/java/ru/danil/lessen2/Main.java
@@ -0,0 +1,12 @@
+package ru.danil.lessen2;
+
+public class Main {
+    public static void   main(String[] args) {
+        Lesson lesson = new Lesson();
+        System.out.println(lesson.within10and20(5, 10));
+        Lesson.isPozitiveOrNegative( 5);
+        System.out.println(Lesson.isNegative(5));
+        Lesson.printWordNTimes("jfjfjf",10);
+    }
+}
+
